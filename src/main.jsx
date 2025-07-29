c5c56e0d4665b0d571d14683988374b3cc91a9f8
Kimgyuilli <rlarbdlf222@gmail.com>
2025-07-29 17:49:18 +0900

    feat: reset, color, font 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles/reset.css'
+import './styles/fonts.css'
+import './styles/colors.css'
 import './index.css'
 import App from './App.jsx'
 
